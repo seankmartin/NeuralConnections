b1d749d218483e862da17f9899dd05d23fa85fb7
Sean Martin <seankieran.m@hotmail.com>
2022-02-16 17:15:40 +0000

    Clean up main control script

--- Code/neuroconnect/control.py
+++ Code/neuroconnect/control.py
@@ -9,7 +9,6 @@ from skm_pyutils.py_config import print_cfg
 from .matrix import main as mouse_main
 from .stats_convergence_rate import main as stats_main
 from .main import main
-from .produce_figures import main as compound_main
 
 
 def run(own_parsed=None):
@@ -41,18 +40,6 @@ def run(own_parsed=None):
             help="Whether to analyse the mouse connectome data",
         )
         parser.add_argument(
-            "-s",
-            "--stats",
-            action="store_true",
-            help="Whether to analyse the stats convergence rate",
-        )
-        parser.add_argument(
-            "-f",
-            "--final",
-            action="store_true",
-            help="Whether to analyse the final plots",
-        )
-        parser.add_argument(
             "-l",
             "--clt_start",
             type=int,
@@ -81,17 +68,6 @@ def run(own_parsed=None):
     if parsed.mouse:
         print("Analysing mouse connectome data")
         pprint(mouse_main())
-        return
-
-    if parsed.stats:
-        print("Analysing stats convergence data")
-        pprint(stats_main(1000, 100, 30, 20000))
-        return
-
-    if parsed.final:
-        result = compound_main()
-        if result is not None:
-            pprint(result)
         return
 
     cfg_path = parsed.cfg
